2928ff8fd1c00d6d674b4288d5bab4a5c10c5983
Sikemig <sikemig@gmail.com>
2024-10-20 11:06:47 +0200

    1234

--- Tema1/Tema1-PSP/src/LanzadorTabla.java
+++ Tema1/Tema1-PSP/src/LanzadorTabla.java
@@ -5,11 +5,12 @@ public class LanzadorTabla {
     public static void main(String[] args) {
         String classpath = System.getProperty("java.class.path");
         // cp y classpath para que funcione la ruta
-        ProcessBuilder proceso = new ProcessBuilder("java" ,"-cp", classpath, "tabla");
+        ProcessBuilder proceso = new ProcessBuilder("java" ,"-cp", classpath, "Tabla");
 
+        proceso.redirectInput(new File("entrada.txt"));
         proceso.redirectError(new File("errores.txt"));
         proceso.redirectOutput(new File("salida.txt"));
-        proceso.redirectInput(new File("entrada.txt"));
+
 
         try{
             proceso.start().waitFor();
